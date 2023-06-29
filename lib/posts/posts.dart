620ddd59a65cc93b29fa76ea7222c0d645002968
Denis Yurchenko <den4ik24@gmail.com>
2023-06-29 19:48:32 +0300

    remake

--- lib/posts/posts.dart
+++ lib/posts/posts.dart
@@ -1,8 +1,4 @@
 export 'bloc/post_bloc.dart';
 export 'models/models.dart';
-// export 'view/view.dart';
-//export 'widgets/widgets.dart';
-export 'package:flutter_infinite_list/posts/view/posts_page.dart';
-export 'package:flutter_infinite_list/posts/view/posts_list.dart';
-export 'package:flutter_infinite_list/posts/widgets/bottom_loader.dart';
-export 'package:flutter_infinite_list/posts/widgets/post_list_item.dart';
+export 'view/view.dart';
+export 'widgets/widgets.dart';
